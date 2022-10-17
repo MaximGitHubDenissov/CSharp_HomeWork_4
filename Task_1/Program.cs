b56d62cad21195ecb8fb82c03d989385b93c42c8
Maxim Denissov <DM7894289@gmail.com>
2022-10-17 14:38:30 +0600

    Modifed Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,15 +4,19 @@
 
 // 2, 4 -> 16
 
-Console.WriteLine ("Введите число A");
-int A = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine ("Введите число B");
-int B = int.Parse (Console.ReadLine()!);
-Console.Write($"{Degree (A,B)} является результатом возведения числа {A} в натуральную степеть числа {B}");
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine()!);
+Console.Write($"{Degree(A, B)} является результатом возведения числа {A} в натуральную степеть числа {B}");
 
-double Degree (int x, int y)
+int Degree(int x, int y)
 {
-    double result = Math.Pow(x,y);
-    return result;
+    int sum = 1;
+    for (int i = 0; i < y; i++)
+    {
+        sum = sum*x;
+    }
+    return sum;
 }
