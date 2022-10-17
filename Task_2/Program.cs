5dc593a3076e2c5ee9f7bdbc0790af894b13a3c2
Maxim Denissov <DM7894289@gmail.com>
2022-10-17 14:31:52 +0600

    Add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine ($"Cумма цифр числа {number} -> {NumbersSum(number)}");
+
+int NumbersSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+
